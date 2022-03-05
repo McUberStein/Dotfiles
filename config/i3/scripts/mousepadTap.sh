35bb95161c5de7a17d3a860612414b2ba4763792
McUberStein <95429589+McUberStein@users.noreply.github.com>
2022-03-05 19:49:35 +0000

    Update mousepadTap.sh

--- config/i3/scripts/mousepadTap.sh
+++ config/i3/scripts/mousepadTap.sh
@@ -1,8 +1,2 @@
-xinput set-prop 9 310 1
-xinput set-prop 10 310 1
-xinput set-prop 11 310 1
-xinput set-prop 12 310 1
-xinput set-prop 13 310 1
-xinput set-prop 14 310 1
-xinput set-prop 15 310 1
+xinput set-prop 'MSFT0001:01 06CB:CD5F Touchpad' 'libinput Tapping Enabled' 1
 
